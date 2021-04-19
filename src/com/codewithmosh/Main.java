7a2c02539db3a6ab1c9b88ec0bf01bda2631addb
unknown <noitegarden@gmail.com>
2021-04-19 20:17:38 -0300

    tratando exceções e limpando tela

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,10 +1,12 @@
 package com.codewithmosh;
 
 import com.boardgame.Board;
+import com.chess.ChessException;
 import com.chess.ChessMath;
 import com.chess.ChessPiece;
 import com.chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,17 +16,27 @@ public class Main {
         ChessMath chessMath = new ChessMath();
 
         while (true) {
-            UI.printBoard(chessMath.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMath.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMath.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMath.performChessMove(source, target);
 
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
